b13db73490f4577bea7f6c43aa240f7901d2196d
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-03 20:37:35 +0700

    updated schema for files without directories

---
+++ prisma/migrations/20240603132718_file/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "path" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
