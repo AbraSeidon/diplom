0fae1ace510be291579e2ea8baf157c945be8015
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-05 02:24:01 +0700

    added File component

---
+++ src/lib/components/File.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let id: string;
+    export let name: string;
+    export let type: string;
+    export let size: number;
+    export let path: string;
+    export let userId: string;
+
+    function handleDownload(e: Event) {
+        e.stopPropagation();
+        downloadFile(id);
+    }
+
+    async function downloadFile(fileId: string) {
+        const res = await fetch("/download?fileId="+fileId);
+        const blob = await res.blob();
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileId);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+</script>
+
+
+
+<div style="border: solid">
+    <p>{name}.{type}</p>
+    <p>{size} bytes</p>
+    <button on:click={handleDownload}>Скачать</button>
+</div>
