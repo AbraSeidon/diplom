4a4304f72d12a26cb3c8834beedea74fd181d547
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-05 20:12:27 +0700

    changed style of some pages

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
 	export let form;
 </script>
-
-<h1>Sign in</h1>
+<!-- 
+<h1>Log in</h1>
 <form method="post">
-	<label for="email">email</label>
+	<label for="email">Email</label>
 	<input name="email" id="email" value={form?.email ?? ''}/><br />
 	<label for="password">Password</label>
 	<input type="password" name="password" id="password" /><br />
 	{#if form?.message}
-		<p style="color: red">{form?.message}</p>
+		<p>{form?.message}</p>
 	{/if}
 	<button>Continue</button>
 </form>
-<a href="/reset-password">Forgot the password</a>
+<a href="/reset-password">Forgot the password</a> -->
+
+<div class="flex flex-col items-center justify-center h-screen">
+	<div class="flex flex-col items-center justify-center min-w-80">
+		<h1 class="text-3xl">Вход</h1>
+		<form method="post" class="w-full">
+			<div class="mb-5">
+				<label for="email" class="block font-medium px-2">Email</label>
+				<input required type="email" name="email" id="email" value={form?.email ?? ''} class="bg-gray-100 rounded-md w-full py-2 px-3 focus:outline-none focus:ring"/>
+			</div>
+			<div class="mb-5">
+				<label for="password" class="block font-medium px-2">Пароль</label>
+				<input  required type="password" name="password" id="password" class="bg-gray-100 rounded-md w-full py-2 px-3 focus:outline-none focus:ring" />
+			</div>
+			{#if form?.message}
+			<p class="text-red-600 text-center font-medium mb-5">{form?.message}</p>
+			{/if}
+			<button type="submit" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center w-full">Войти</button>
+		</form>
+		<a href="/reset-password" class="underline">Забыли пароль?</a>
+		<p>Ещё нет аккунта? <a href="/signup" class="underline">Зарегистрируйтесь</a> </p>
+	</div>
+</div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,17 +2,27 @@
 	export let form;
 
 </script>
-
-<h1>Sign up</h1>
-<form method="post">
-	<label for="email">email</label>
-	<input name="email" id="email" value={form?.email ?? ''}/> <br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /> <br />
-	<label for="passwordConfirmation">Password confirmation</label>
-	<input type="password" name="passwordConfirmation" id="passwordConfirmation"> <br />
-	{#if form?.message}
-		<p style="color: red">{form?.message}</p>
-	{/if}   
-	<button>Continue</button>
-</form>
+<div class="flex flex-col items-center justify-center h-screen">
+	<div class="flex flex-col items-center justify-center min-w-80">
+		<h1 class="text-3xl">Регистрация</h1>
+		<form method="post" class="w-full">
+			<div class="mb-5">
+				<label for="email" class="block font-medium px-2">Email</label>
+				<input required type="email" name="email" id="email" value={form?.email ?? ''} class="bg-gray-100 rounded-md w-full py-2 px-3 focus:outline-none focus:ring"/>
+			</div>
+			<div class="mb-5">
+				<label for="password" class="block font-medium px-2">Пароль</label>
+				<input required type="password" name="password" id="password" class="bg-gray-100 rounded-md w-full py-2 px-3 focus:outline-none focus:ring" />
+			</div>
+			<div class="mb-5">
+				<label for="passwordConfirmation" class="block font-medium px-2">Повторите пароль</label>
+				<input required type="password" name="passwordConfirmation" id="passwordConfirmation" class="bg-gray-100 rounded-md w-full py-2 px-3 focus:outline-none focus:ring" />
+			</div>
+			{#if form?.message}
+			<p class="text-red-600 text-center font-medium mb-5">{form?.message}</p>
+			{/if}
+			<button type="submit" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center w-full">Зарегистрироваться</button>
+		</form>
+		<p>Уже зарегистрированы? <a href="/login" class="underline">Войти</a> </p>
+	</div>
+</div>
