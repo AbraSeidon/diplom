588d17fb11df2a2d24ac362a82a4e9cfb670522c
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-08 00:55:10 +0700

    Now you can download files

--- src/lib/components/File/File.svelte
+++ src/lib/components/File/File.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     export let id: string;
     export let name: string;
     export let type: string;
     export let size: number;
+    
+    let fileUrl: string; 
 
+    onMount(() => fileUrl = window.location.href + "/" + name);
 
     function handleDownload(e: Event) {
         e.stopPropagation();
@@ -23,16 +28,37 @@
     }
 </script>
 
-<div style="border: solid">
-    {#if type === "dir"}
-        <p>{name}</p>
-    {:else}
-        <p>{name}.{type}</p>
-        <p>{size} bytes</p>
-        <button on:click={handleDownload}>Скачать</button>
-    {/if}
-    <form action="?/delete" method="POST">
-        <input type="hidden" name="id" value={id}>
-        <button type="submit">Удалить</button>
-    </form>
+<div class="border rounded-md shadow-md m-5">
+    <div class="grid md:grid-cols-4 grid-cols-2">
+        {#if type === "dir"}
+        <div class="col-span-2">
+            <p>{name}</p>
+        </div>
+        <div>
+            <a href={fileUrl} class="button bg-black text-white rounded-md py-1 px-2 text-center">Открыть</a>
+        </div>
+        <div>
+            <form action="?/delete" method="POST">
+                <input type="hidden" name="id" value={id}>
+            <button type="submit" class="button bg-black text-white rounded-md py-1 px-2 text-center">Удалить</button>
+            </form>
+        </div>
+        {:else}
+        <div>
+            <p>{name}.{type}</p>
+        </div>
+        <div>
+            <p>{size} bytes</p>
+        </div>
+        <div>
+            <button on:click={handleDownload} class="button bg-black text-white rounded-md py-1 px-2 text-center">Скачать</button>
+        </div>
+        <div>
+            <form action="?/delete" method="POST">
+                <input type="hidden" name="id" value={id}>
+                <button type="submit" class="button bg-black text-white rounded-md py-1 px-2 text-center">Удалить</button>
+            </form>
+        </div>
+{/if}
+    </div>
 </div>
