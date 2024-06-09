6fc9716ad52bbbc8dabd0002e1420c1c7f129c99
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-10 00:00:51 +0700

    install lucide, now with icons

--- src/lib/components/File/File.svelte
+++ src/lib/components/File/File.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { Folder, File } from "lucide-svelte"
+    import { goto } from "$app/navigation";
 
     export let id: string;
     export let name: string;
@@ -31,8 +33,9 @@
 <div class="border rounded-md shadow-md mb-5">
     <div class="grid md:grid-cols-4 grid-cols-2 gap-x-5 gap-y-1">
         {#if type === "dir"}
-            <div class="col-span-2 flex flex-col justify-center p-3">
-                <p>{name}</p>
+            <div class="col-span-2 gap-2 flex items-center p-3">
+                <Folder size={36} fill={"#000"} />
+                <span>{name}</span>
             </div>
             <div class="flex flex-col justify-center p-3">
                 <a href={fileUrl} class="button bg-black text-white rounded-md py-1 px-2 text-center">Открыть</a>
@@ -40,23 +43,24 @@
             <div class="flex flex-col justify-center p-3">
                 <form action="?/delete" method="POST" class="text-center">
                     <input type="hidden" name="id" value={id}>
-                    <button type="submit" class="button bg-black text-white rounded-md py-1 px-2 text-center w-full">Удалить</button>
+                    <button on:click|stopPropagation type="submit" class="button bg-black text-white rounded-md py-1 px-2 text-center w-full">Удалить</button>
                 </form>
             </div>
         {:else}
-            <div class="flex flex-col justify-center p-3">
-                <p>{name}.{type}</p>
+            <div class="flex gap-2 items-center p-3">
+                <File size={36}/>
+                <span>{name}.{type}</span>
             </div>
-            <div class="flex flex-col justify-center p-3">
+            <div class="flex items-center p-3">
                 <p>{size} bytes</p>
             </div>
-            <div class="flex flex-col justify-center p-3">
-                <form class="text-center">
+            <div class="flex items-center p-3">
+                <form class="text-center w-full">
                     <button on:click={handleDownload} class="button bg-black text-white rounded-md py-1 px-2 text-center w-full">Скачать</button>
                 </form>
             </div>
-            <div class="flex flex-col justify-center p-3">
-                <form action="?/delete" method="POST" class="text-center">
+            <div class="flex items-center p-3">
+                <form action="?/delete" method="POST" class="text-center w-full">
                     <input type="hidden" name="id" value={id}>
                     <button type="submit" class="button bg-black text-white rounded-md py-1 px-2 text-center w-full">Удалить</button>
                 </form>
@@ -64,9 +68,3 @@
         {/if}
     </div>
 </div>
-
-<style>
-    /* * {
-        border: solid green
-    } */
-</style>
