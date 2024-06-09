440d40adc3fe7158bc63ec1ee054d3b044c9735b
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-10 01:25:33 +0700

    now with header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,17 @@
-<slot></slot><script>import "../app.css";</script><style></style>
+<script lang="ts">
+    import "../app.css";
+    export let data;
+</script>
+
+<div class="flex flex-col h-screen">
+    <div class="bg-gray-300 flex justify-between pr-10 pl-10 md:pl-5 items-center py-5">
+        <div>
+            <a href="/" class="text-5xl font-medium">ddisk</a>
+        </div>
+        <div>
+            <a href="/profile" class="button bg-black text-white rounded-md text-lg px-2 py-1 {data.user? '' : 'hidden'}">Профиль</a>
+        </div>
+    </div>
+    <slot></slot>   
+</div>
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen">
+<div class="flex flex-col items-center justify-center border grow">
     <h1 class="text-lg">Добро пожаловать в</h1>
     <h1 class="text-7xl font-medium tracking-[.5em] mr-[-.5em]">ddisk</h1>
     <h2 class="text-lg">средство для хранения файлов</h2>
     <div class="mx-12 my-5">
-    <div class="grid grid-cols-2 gap-4">    
-        <a href="/signup" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center">Регистрация</a>
-        <a href="/login" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center">Вход</a>
+        <div class="grid grid-cols-2 gap-4">    
+            <a href="/signup" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center">Регистрация</a>
+            <a href="/login" class="button bg-black text-white rounded-md text-lg py-2 px-3 text-center">Вход</a>
+        </div>
     </div>
-</div>
 </div>
 
