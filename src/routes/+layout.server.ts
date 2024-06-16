c59adf8c7c5d03effac335afc2184fece41e1b99
Kirill Dryagin <129253739+AbraSeidon@users.noreply.github.com>
2024-06-16 20:47:28 +0700

    console.log process.cwd

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
+
+    console.log(process.cwd());
     return {
         user: locals.user
     }
